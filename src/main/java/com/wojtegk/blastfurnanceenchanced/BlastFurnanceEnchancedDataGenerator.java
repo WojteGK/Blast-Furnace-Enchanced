599a3bf9eb61eb20ae075f7c63c761e7f3b46220
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-05-13 13:36:11 +0200

    initial commit

---
+++ src/main/java/com/wojtegk/blastfurnanceenchanced/BlastFurnanceEnchancedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.wojtegk.blastfurnanceenchanced;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlastFurnanceEnchancedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
